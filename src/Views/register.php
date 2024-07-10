cf1be43e5019a78f48488edc1dc4b15fff37ad2d
NlcmarquesDev <nunolcmarques@gmail.com>
2024-07-10 16:33:03 +0200

    busy with register page

---
+++ src/Views/register.php
@@ -0,0 +1,97 @@
+<?php include 'src/Views/partials/header.php'; ?>
+
+<!-- Navbar & Hero Start -->
+<div class="container-xxl position-relative p-0">
+    <?php include 'src/Views/partials/navbar.php'; ?>
+
+    <div class="container-xxl py-5 bg-dark hero-header mb-5">
+        <div class="container text-center my-5 pt-5 pb-4">
+            <h1 class="display-3 text-white mb-3 animated slideInDown">Register</h1>
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb justify-content-center text-uppercase">
+                    <li class="breadcrumb-item"><a href="/pizzawinkel_app/">Home</a></li>
+                    <li class="breadcrumb-item text-white active" aria-current="page">register</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+</div>
+<!-- Navbar & Hero End -->
+
+
+<div class=" px-4 py-5 my-auto">
+    <div class="row row-cols-1 row-cols-md-2 align-items-md-center g-5 py-5">
+        <div class="col d-flex flex-column align-items-start gap-2">
+            <h4 class="text-body-secondary fst-italic fw-bold">"Join the Family! Taste the Difference with Every Slice."</h4>
+        </div>
+
+        <div class="col">
+            <main class="form-signin w-100 m-auto">
+                <h1 class="h3 mb-3 fw-normal">Please Sign in</h1>
+                <form method="POST" action="/notes_app_php/register">
+                    <div class="row row-cols-1 row-cols-md-2 g-4">
+                        <div class="form-floating  col">
+                            <input type="text" name="firstname" class="form-control" id="floatingInput" placeholder="name@example.com">
+                            <label class="ps-4" for="floatingInput">First Name</label>
+                        </div>
+                        <div class="form-floating col">
+                            <input type="text" name="lastname" class="form-control" id="floatingInput" placeholder="name@example.com">
+                            <label class="ps-4" for="floatingInput">Last Name</label>
+                        </div>
+                        <div class="form-floating col">
+                            <input type="text" name="adress" class="form-control" id="floatingInput" placeholder="adress">
+                            <label class="ps-4" for="floatingInput">Adress</label>
+                        </div>
+                        <div class="form-floating col">
+                            <input type="text" name="city" class="form-control" id="floatingInput" placeholder="city">
+                            <label class="ps-4" for="floatingInput">City</label>
+                        </div>
+                        <div class="form-floating col">
+                            <input type="text" name="postcode" class="form-control" id="floatingInput" placeholder="postcode">
+                            <label class="ps-4" for="floatingInput">Postcode</label>
+                        </div>
+                        <div class="form-floating col ">
+                            <input type="text" name="phone" class="form-control" id="floatingInput" placeholder="Phone number">
+                            <label class="ps-4" for="floatingInput">Phone number</label>
+                        </div>
+                        <div class="form-check form-switch mx-auto">
+                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
+                            <label class="form-check-label" for="flexSwitchCheckChecked">I want to create a new account</label>
+                        </div>
+
+
+
+
+
+
+
+
+
+
+                        <div id="check-user-database " class="d-none">
+                            <div class="form-floating ">
+                                <input type="email" name="email" class="form-control mb-3" id="floatingInput" placeholder="name@example.com">
+                                <label for="floatingInput">Email address</label>
+                            </div>
+                            <div class="form-floating ">
+                                <input type="password" name="password" class="form-control mb-3" id="floatingPassword" placeholder="Password">
+                                <label for="floatingPassword">Password</label>
+                            </div>
+                        </div>
+                        <?php if (isset($_SESSION['errors'])) : ?>
+                            <div class="alert alert-danger fw-light fs-6 " role="alert">
+                                <?= $_SESSION['errors'] ?>
+                            </div>
+                        <?php endif  ?>
+                        <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+
+                    </div>
+                </form>
+            </main>
+        </div>
+    </div>
+</div>
+
+
+<!-- Footer Start -->
+<?php include 'src/Views/partials/footer.php'; ?>
