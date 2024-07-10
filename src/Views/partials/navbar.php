208e60b6dc326c65d9e2ab4445f89a677bc81648
NlcmarquesDev <nunolcmarques@gmail.com>
2024-07-10 12:08:14 +0200

    make the offcanvas and the login and register pages design

--- src/Views/partials/navbar.php
+++ src/Views/partials/navbar.php
@@ -13,7 +13,33 @@
             <a href="menu.php" class="nav-item nav-link">Menu</a>
             <a href="contact.php" class="nav-item nav-link">Contact</a>
         </div>
-        <a href="" class="btn btn-primary py-1 px-2 me-3">SignUp</a>
-        <a href="" class="btn btn-primary py-1 px-2">Register</a>
+        <a href="/pizzawinkel_app/login.php" class="btn btn-primary py-1 px-2 me-3">SignUp</a>
+        <a href="/pizzawinkel_app/register.php" class="btn btn-primary py-1 px-2 me-3">Register</a>
+
+
+        <a class="btn btn-primary position-relative py-1 px-2" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+            <i class="bi bi-bag"></i>
+            <?php if (isset($_SESSION['card'])) : ?>
+                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                    99+
+                    <span class="visually-hidden">unread messages</span>
+                </span>
+            <?php endif; ?>
+
+        </a>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasExampleLabel">Your Order</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <div>
+                    Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+                </div>
+                <div class="dropdown mt-3">
+
+                </div>
+            </div>
+        </div>
     </div>
 </nav>
